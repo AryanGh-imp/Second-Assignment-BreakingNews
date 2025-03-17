58a7ba10b01533dd9f858075de838fa1e8b0bcd0
AryanGh-imp <aryan.ghasemi.imp@gmail.com>
2025-03-17 23:28:03 +0330

    Config.java code added & .gitignore fixed

--- BreakingNews/src/main/java/AP/Main.java
+++ BreakingNews/src/main/java/AP/Main.java
@@ -2,6 +2,16 @@ package AP;
 
 public class Main {
     public static void main(String[] args) {
+        try {
+            // Get API Key from Config
+            String apiKey = Config.getApiKey();
+            System.out.println("Using API Key: " + apiKey);
 
+            // Continue running the program...
+
+        }
+        catch (RuntimeException e) {
+            System.err.println(e.getMessage());
+        }
     }
 }
